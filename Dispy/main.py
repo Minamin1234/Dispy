ce00b9127986190f714d32fa5841bab7bae97bfd
Minamin <62072288+Minamin1234@users.noreply.github.com>
2022-01-24 04:00:08 +0900

    開発者用コマンドの実装。

--- Dispy/main.py
+++ Dispy/main.py
@@ -2,6 +2,7 @@ import discord
 from Dispy import MDispy
 from Dispy import DDev
 
+Developer_Id:int = 834758459349925939
 
 client = discord.Client();
 Token = 'ODgzMzc4MTAzNjM4OTYyMjU2.YTJD-A.6lmBAZzWQ98o6Ve9cXWkEEuxp6g'
@@ -25,6 +26,9 @@ async def on_message(message):
         CommandDevice.SetMsg(message)
         result:str = CommandDevice.Execute(word)
         if args[0] == "dev":
+            if message.author.id != Developer_Id:
+                await message.channel.send("Can't execute command: Developer Only")
+                return
             if args[1] == "send":
                 await message.channel.send(str(args[2]))
                 return
