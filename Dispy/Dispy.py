01cb8423fc1591f28323b98f28f8e8b68c3269f8
Minamin <minamin37345@gmail.com>
2022-01-24 02:22:50 +0900

    開発者用コマンドの定義．

--- Dispy/Dispy.py
+++ Dispy/Dispy.py
@@ -1,7 +1,64 @@
 from typing import NoReturn as void
 from typing import List
+
+from discord.client import Client
 from Compy import MCommand
+from Compy import MModule
 import discord
 
+class DModule(MModule):
+    Disbot:discord.Client = None
+    msg:discord.Message = None
+
+    def __init__(self) -> void:
+        self.ModuleName = "dmodule"
+        return
+
+class DDev(DModule):
+    def __init__(self,disbot:discord.Client) -> void:
+        super().__init__()
+        self.Disbot = disbot
+        self.ModuleName = "dev"
+        self.Commands = [
+            "send",
+            "sendall",
+            "sendto",
+            "stop",
+            "help"
+            ]
+        return
+
+    def ExecuteCommand(self,args:List[str]) -> str:
+        result:str = ""
+        if args[1] == self.Commands[0]:
+            #dev.send([text])
+            result = str(args[2])
+        elif args[1] == self.Commands[1]:
+            #dev.sendall([text])
+            result = str(args[2])
+        elif args[1] == self.Commands[2]:
+            #dev.sendto([text],[channel_Id])
+            result = str(args[2])
+        elif args[1] == self.Commands[3]:
+            #dev.stop()
+            result = "stop..."
+            self.Disbot.loop.stop()
+        elif args[1] == self.Commands[4]:
+            #dev.help()
+            result = self.ShowHelp()
+        return result
+
 class MDispy(MCommand):
-    pass
+    msg:discord.Message = None
+    def __init__(self) -> void:
+        super().__init__()
+        client = discord.Client()
+        return
+
+    def SetMsg(self,newmsg:discord.Message):
+        self.msg = newmsg
+        for module in self.Modules:
+            if isinstance(module,DModule):
+                module.msg = newmsg
+        return
+
--- Dispy/main.py
+++ Dispy/main.py
@@ -1,10 +1,15 @@
 import discord
 from Dispy import MDispy
+from Dispy import DDev
+
 
 client = discord.Client();
 Token = 'ODgzMzc4MTAzNjM4OTYyMjU2.YTJD-A.6lmBAZzWQ98o6Ve9cXWkEEuxp6g'
 CommandWord:str = "!c "
 CommandDevice:MDispy = MDispy()
+ddev:DDev = DDev(client)
+CommandDevice.IncludeNewModule(ddev)
+
 
 @client.event
 async def on_ready():
@@ -15,8 +20,11 @@ async def on_message(message):
     if message.author.bot:
         return
     if message.content.startswith(CommandWord):
-        word:str = message.content.lstrip("!c ")
+        word:str = message.content.lstrip(CommandWord)
+        args:List[str] = CommandDevice.DecodeArgs(word)
+
+        CommandDevice.SetMsg(message)
         result:str = CommandDevice.Execute(word)
-        await message.channel.send(result)
+        await message.channel.send(">>" + str(result))
 
 client.run(Token)
